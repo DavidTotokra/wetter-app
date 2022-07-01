14d4bd8f7ec6dddf64baf8160c76f7630a7c655f
DavidTotokra <102921842+DavidTotokra@users.noreply.github.com>
2022-07-01 14:00:44 +0200

    Update Api.ts

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,4 +1,4 @@
 
 
 export const API_URL=
-`https://api.openweathermap.org/data/2.5/onecall?appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY="40b0274c737baab438890c3a182f6aca"}&exclude=minutely,alerts`;
+`https://api.openweathermap.org/data/2.5/onecall?appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY="?????"}&exclude=minutely,alerts`;
