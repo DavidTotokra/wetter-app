0a2289fc5d2716fbc0ce8db31ba5769c8a65a091
DavidTotokra <102921842+DavidTotokra@users.noreply.github.com>
2022-07-01 13:59:17 +0200

    Update Ort_Api.ts

--- src/services/Ort_Api.ts
+++ src/services/Ort_Api.ts
@@ -1,7 +1,3 @@
 
 export const Standort_API = 
-`https://api.locationiq.com/v1/autocomplete.php?key=pk.58f6fdb7cb8aec9a095b9f7811f3bcf6&q=paris&limit=5`;
-
-/**
- * api.openweathermap.org/data/2.5/forecast?id=524901&appid={40b0274c737baab438890c3a182f6aca}
- */
+`https://api.locationiq.com/v1/autocomplete.php?key=?????=paris&limit=5`;
